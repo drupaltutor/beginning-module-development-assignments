829b000d51e712a3b1662cf617d125eacea32492
Wayne Eaker <wayne@zengenuity.com>
2023-02-11 11:08:00 -0500

    Updated /classes/{class_id} and /classes/{class_id}/roster routes to use data fetched from the API

--- class_info/src/Controller/ClassInfoController.php
+++ class_info/src/Controller/ClassInfoController.php
@@ -2,31 +2,66 @@
 
 namespace Drupal\class_info\Controller;
 
+use Drupal\class_info\ClassInfoManagerInterface;
 use Drupal\Component\Utility\Html;
+use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class ClassInfoController extends ControllerBase {
 
+  /**
+   * Static cache for infomation about the classes.
+   *
+   * @var array
+   */
+  protected array $classInfo = [];
+
+  /**
+   * @var ClassInfoManagerInterface
+   */
+  protected ClassInfoManagerInterface $classInfoManager;
+
+  public function __construct(ClassInfoManagerInterface $class_info_manager) {
+    $this->classInfoManager = $class_info_manager;
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('class_info.manager')
+    );
+  }
+
   public function overview(string $class_id) {
+    $class_info = $this->getClassInfo($class_id);
     $build = [];
     $build['class'] = [
       '#type' => 'html_tag',
       '#tag' => 'h2',
-      '#value' => $this->t('Course: @course', ['@course' => $class_id]),
+      '#value' => $this->t('Course: @course', ['@course' => $class_info['id']]),
     ];
     $build['teacher'] = [
       '#type' => 'html_tag',
       '#tag' => 'h2',
-      '#value' => $this->t('Teacher: @teacher', ['@teacher' => 'John Doe']),
+      '#value' => $this->t('Teacher: @first_name @last_name', [
+        '@first_name' => $class_info['teacher']['first_name'],
+        '@last_name' => $class_info['teacher']['last_name'],
+      ]),
     ];
     return $build;
   }
 
   public function overviewTitle(string $class_id) {
-    return Html::escape($class_id);
+    $class_info = $this->getClassInfo($class_id);
+    return $class_info['name'];
+  }
+
+  public function overviewAccess(string $class_id) {
+    return AccessResult::allowedIf(!empty($this->getClassInfo($class_id)));
   }
 
   public function roster(string $class_id) {
+    $class_info = $this->getClassInfo($class_id);
     $build = [];
     $build['title'] = [
       '#type' => 'html_tag',
@@ -37,24 +72,14 @@ class ClassInfoController extends ControllerBase {
       $this->t('Last Name'),
       $this->t('First Name'),
     ];
-    $rows = [
-      [
-        'Mayer',
-        'Anastasia',
-      ],
-      [
-        'Miles',
-        'Samuel',
-      ],
-      [
-        'Randolph',
-        'Brooklyn',
-      ],
-      [
-        'Savage',
-        'Cody',
-      ],
-    ];
+    $rows = [];
+    foreach ($class_info['roster'] as $student) {
+      $rows[] = [
+        $student['last_name'],
+        $student['first_name'],
+      ];
+    }
+
     $build['roster'] = [
       '#theme' => 'table',
       '#header' => $header,
@@ -64,7 +89,29 @@ class ClassInfoController extends ControllerBase {
   }
 
   public function rosterTitle(string $class_id) {
-    return $this->t('Class Roster: @class', ['@class' => $class_id]);
+    $class_info = $this->getClassInfo($class_id);
+    return $this->t('Class Roster: @class', ['@class' => $class_info['id']]);
+  }
+
+  public function rosterAccess(string $class_id) {
+    return AccessResult::allowedIf(!empty($this->getClassInfo($class_id)));
+  }
+
+  /**
+   * Gets the class info either from the static cache or from the API
+   *
+   * @param string $class_id
+   * @return array|bool
+   */
+  protected function getClassInfo(string $class_id) {
+    if (!isset($this->classInfo[$class_id])) {
+      $class_info = $this->classInfoManager->getClassInfo($class_id);
+      if ($class_info === NULL) {
+        $class_info = FALSE;
+      }
+      $this->classInfo[$class_id] = $class_info;
+    }
+    return $this->classInfo[$class_id];
   }
 
 }
